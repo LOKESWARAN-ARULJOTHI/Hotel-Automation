3d25629d5587390dcc8a68a8d25fde7eb46e1cf2
Lokeswaran Aruljothi <74011196+LOKESWARAN-ARULJOTHI@users.noreply.github.com>
2022-04-01 02:56:55 +0530

    Update Corridor.java

--- src/main/java/Corridor.java
+++ src/main/java/Corridor.java
@@ -3,6 +3,8 @@ public class Corridor {
     protected String airConditioner;
     protected String light;
 
+    
+    
     public Corridor(String light, String airConditioner, int corridorNumber) {
         this.light = light;
         this.airConditioner = airConditioner;
